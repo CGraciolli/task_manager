5d2d968bb64a0a488462f140a93b9177c044ab98
CGraciolli <cgraciolli@gmail.com>
2023-03-23 15:26:38 +0100

    users

---
+++ task_manager/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+    
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your Username',
+        'class': 'rounded border-0 col-6',
+        }))
+
+    email= forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Your Email',
+        'class': 'rounded border-0 col-6',
+        }))
+    
+    password1= forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your Password',
+        'class': 'rounded border-0 col-6',
+        }))
+
+    password2= forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password',
+        'class': 'rounded border-0 col-6',
+        }))
+    
+class LogInForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your Username',
+        'class': 'rounded border-0 col-6',
+        }))
+    
+    password= forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your Password',
+        'class': 'rounded border-0 col-6',
+        }))  
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,1 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+
+def tasks(request):
+    return render(request, "tasks.html", {})
+
+def item(request):
+    return render(request, "item.html", {})
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect("/login/")
+    else:
+        form = SignUpForm()
+
+    return render(request, "signup.html", {"form": form})
